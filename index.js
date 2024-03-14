6996418fae89ae42e1eacdf10d29165732e9f82c
rkokhan-motorfyx <rodionkokhanmotorfyx@gmail.com>
2024-03-14 17:33:00 +0200

    reuse isolate

--- index.js
+++ index.js
@@ -2,48 +2,68 @@ import ivm from 'isolated-vm';
 import { createRandomVehicles, getDetectors } from './generateTestData.js';
 import { performance, PerformanceObserver } from "node:perf_hooks";
 
-const LEAD_AMOUNT = 1000;
-
-
-
-const processLeads = async (lead, script) => {
-    const isolate = new ivm.Isolate({ 
-        inspector:false,
-        //mb
-        memoryLimit: 64,
-        onCatastrophicError: (err) => {
-            console.log('Catastrophic Error: %s', err);
-        },
-    });
-
-    const context = await isolate.createContext({});
-
-    const jail = context.global;
-    await jail.set('global', jail.derefInto({}));
-    await jail.set('log', function(...args) {
-        console.log("log called");
-        console.log(...args);
-    });
-
-    // const derefInfo = jail.derefInto(lead);
-    await jail.set("getLeads", function(){
-        return [lead];
-    });
-
-    await jail.set("setResult", function(result){
-        // console.log("Result: %s", JSON.stringify(result));
-    });
-
-    await context.eval(script).catch((err)=>{
-        console.log('Error: %s', err ?? "Unknown error");
-    });
-    
-
-    context.release();
-};
+const LEAD_AMOUNT = 10000;
 
 const leads = createRandomVehicles(LEAD_AMOUNT);
 const scripts = getDetectors();
+
+
+const isolatePool = scripts.map(() => new ivm.Isolate({ 
+    inspector:false,
+    //mb
+    memoryLimit: 64,
+    onCatastrophicError: (err) => {
+        console.log('Catastrophic Error: %s', err);
+    },
+}));
+
+
+const processLeads = async (lead, scripts) => {
+    const promises = isolatePool.map(async (_, i) => {
+        const isolate = isolatePool[i];
+        const script = scripts[i];
+
+        performance.mark(`virtual_run_start_${i}`);
+        const context = await isolate.createContext({
+            inspector:false,
+        });
+
+        const jail = context.global;
+        await jail.set('global', jail.derefInto({}));
+        await jail.set('log', function(...args) {
+            console.log("log called");
+            console.log(...args);
+        });
+
+        // const derefInfo = jail.derefInto(lead);
+        await jail.set("getLeads", function(){
+            return [lead];
+        });
+
+        await jail.set("setResult", function(result){
+            // console.log("Result: %s", JSON.stringify(result));
+        });
+
+        const compiled = await isolate.compileScript(script);
+
+        await compiled.run(context).catch((err)=>{
+            console.log('Error: %s', err ?? "Unknown error");
+        });
+
+        // await context.eval(script).catch((err)=>{
+        //     console.log('Error: %s', err ?? "Unknown error");
+        // });
+
+        compiled.release();
+        context.release();
+
+
+        performance.mark(`virtual_run_end_${i}`);
+        performance.measure(`virtual_run_${i}`, `virtual_run_start_${i}`, `virtual_run_end_${i}`);
+    });
+
+    await Promise.all(promises);
+};
 
 const dataCollection = {};
 
@@ -70,13 +90,13 @@ for(let d = 0; d < leads.length; d++){
     const lead = leads[d];
     console.log("Processing lead %d of %d", d, leads.length);
     performance.mark('v_start');
-    for(let i = 0; i < scripts.length; i++){
-        const script = scripts[i];
-        performance.mark(`virtual_run_start_${i}`);
-        await processLeads(lead, script);
-        performance.mark(`virtual_run_end_${i}`);
-        performance.measure(`virtual_run_${i}`, `virtual_run_start_${i}`, `virtual_run_end_${i}`);
-    }
+    // for(let i = 0; i < scripts.length; i++){
+    //     const script = scripts[i];
+    //     performance.mark(`virtual_run_start_${i}`);
+        await processLeads(lead, scripts);
+    //     performance.mark(`virtual_run_end_${i}`);
+    //     performance.measure(`virtual_run_${i}`, `virtual_run_start_${i}`, `virtual_run_end_${i}`);
+    // }
     performance.mark('v_end');
     performance.measure('v_run', 'v_start', 'v_end');
 }
@@ -85,6 +105,7 @@ for(let d = 0; d < leads.length; d++){
 
 performance.mark('run_end');
 performance.measure('run', 'run_start', 'run_end');
+performance.clearMarks();
 
 
 Object.entries(dataCollection).map(([n, values])=> {
